972e3b349e9e1632db5adf9132477851d8bba462
zackynson <crys.chb@hotmail.com>
2020-01-30 14:28:07 -0300

    update method added on RecipientController

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -8,9 +8,62 @@ class RecipientController {
       return res.status(401).json({ error: 'user not found' });
     }
 
-    const recipient = await Recipient.create(req.body);
+    const {
+      id,
+      name,
+      street_name,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await Recipient.create(req.body);
 
-    return res.json(recipient);
+    return res.json({
+      id,
+      name,
+      street_name,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
+  }
+
+  async update(req, res) {
+    const { id } = req.params;
+
+    const user = await User.findByPk(req.userId);
+
+    if (!user) {
+      return res.status(401).json({ error: 'user not found' });
+    }
+
+    const recipient = await Recipient.findByPk(id);
+    if (!recipient) {
+      return res.status(400).json({ error: 'recipient not found' });
+    }
+    const {
+      name,
+      street_name,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    } = await recipient.update(req.body);
+
+    return res.json({
+      id: +id,
+      name,
+      street_name,
+      number,
+      complement,
+      state,
+      city,
+      cep,
+    });
   }
 }
 
