884e323d833995bfce85c8536a69265a0ea29d4c
zackynson <crys.chb@hotmail.com>
2020-01-30 14:59:40 -0300

    jwt fix and input validations

--- src/app/controllers/RecipientController.js
+++ src/app/controllers/RecipientController.js
@@ -1,9 +1,28 @@
+import * as Yup from 'yup';
 import Recipient from '../models/Recipient';
 import User from '../models/User';
 
 class RecipientController {
   async store(req, res) {
+    const schema = Yup.object().shape({
+      name: Yup.string().required(),
+      street_name: Yup.string().required(),
+      number: Yup.number().required(),
+      complement: Yup.string().notRequired(),
+      state: Yup.string()
+        .required()
+        .min(2)
+        .max(2),
+      city: Yup.string().required(),
+      cep: Yup.number().required(),
+    });
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'validation fails' });
+    }
+
     const user = await User.findByPk(req.userId);
+
     if (!user) {
       return res.status(401).json({ error: 'user not found' });
     }
@@ -44,6 +63,7 @@ class RecipientController {
     if (!recipient) {
       return res.status(400).json({ error: 'recipient not found' });
     }
+
     const {
       name,
       street_name,
--- src/app/middlewares/auth.js
+++ src/app/middlewares/auth.js
@@ -13,8 +13,8 @@ export default async (req, res, next) => {
   try {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
     req.userId = decoded.id;
+    return next();
   } catch (error) {
     return res.status(401).json({ error: 'invalid token' });
   }
-  return next();
 };
